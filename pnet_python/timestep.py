467a4893e78f2bdd9f580d4e2a7c56353de954fc
Jack-Hastings <Jack.Hastings@gmail.com>
2020-05-24 17:39:06 -0400

    play

--- pnet_python/timestep.py
+++ pnet_python/timestep.py
@@ -3,9 +3,19 @@ import pandas
 from input import climate_df
 
 climlength = len(climate_df) #length of climate record 309 years * 12 months
+clim_ln = int(len(climate_df)/12)
 
-climate_df[1][1]
+rstep = 2
 
+print (climate_df.iloc[1,rstep])
 
-for rstep in range(climlength):
-    print(rstep)
+'''
+A note to self: as an iterator for a dataframe, using range works well 
+because it goes from 0. But, keep this in mind if using for other stuff. 
+This is likely, why we see rstep + 1 in Zaixing's code. 
+'''
+#for rstep in range(clim_ln):
+#    print(rstep)
+#tave = (tmin + tmax)/2
+#tday = (tmax + tday)/2
+#tnight = (tave + tmin)/2
