c7c32bb3e61e63a30385e5b811dd468b9a1450f4
Jack-Hastings <Jack.Hastings@gmail.com>
2020-05-29 15:14:52 -0400

    starting to input phenology

--- pnet_python/Photosyn.py
+++ pnet_python/Photosyn.py
@@ -1,1 +1,21 @@
-import input
+from AtmEnviron import getdays
+from pnet_input import *
+
+def phenology(rstep, share, growthphase):
+    '''Phenology calculations for the PnET ecosystem model '''
+
+    #still not sure best way to transfer over climate rstep variables
+    doy = climate.loc[rstep,'DOY']
+    year = climate.loc[rste, 'Year']
+
+    if growthphase == 1:
+        dayspan = getdays(doy, year)
+
+        gdd = share['tave'] * dayspan
+        tave_yr = 
+
+    '''Assign newly created variables to share dictionary'''
+        share['dayspan'] = dayspan
+
+
+
