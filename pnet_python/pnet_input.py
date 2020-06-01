9fa1566e9c3f919c49176d60c5ecf088e54feeae
Jack-Hastings <Jack.Hastings@gmail.com>
2020-06-01 11:55:06 -0400

    ripped out input if/else; added comments

--- pnet_python/pnet_input.py
+++ pnet_python/pnet_input.py
@@ -10,28 +10,22 @@ import pandas as pd
 path = pathlib.Path.cwd() 
 path_input = path.parent / 'Input' 
 
-'''
-Read in input and climate file.
-I could rip out the if/else stuff. Not necessary 
-'''
+'''Read in input and climate file.'''
 #Input text file. 
-if pathlib.Path.exists(path_input / 'pynet_input.csv'):
-    input = pd.read_csv(path_input / 'pynet_input.csv')
-else:
-    print('input file does not exist')
+input = pd.read_csv(path_input / 'pynet_input.csv')
+
 #Climate clim file
-if pathlib.Path.exists(path_input / 'climate.clim'):
-    climate = pd.read_table(path_input / 'climate.clim')
-else:
-    print('climate file does not exist')
+climate = pd.read_table(path_input / 'climate.clim')
+
 
 '''assign a climate length to be used in the rstep looping'''
 clim_length = len(climate) 
 
 
-'''
-Below I assign the input data to dictionaries to mimic the transparency 
-of the c++ structs. This may or may not be a good idea. Can easily rip it out.
+'''Assign the input data to dictionaries
+
+I've done this to mimic the transparency of the c++ structs. 
+This may or may not be a good idea. Can easily rip it out.
 '''
 #model options
 modeloptions = input.iloc[0:2].set_index('variable')['value'].to_dict()
@@ -43,19 +37,17 @@ tree_settings = input.iloc[23:78].set_index('variable')['value'].to_dict()
 management_settings = input.iloc[79:].set_index('variable')['value'].to_dict()
 #Notice the index pattern here -- seems like [79:] is wrong -- but it works. 
 
-#Having the data in this structure would allow us to write transparent stuff, like:
-example = tree_settings['RootTurnoverA'] * tree_settings['WoodTurnover']
-
 
 '''Define share dictionary to hold the outputs of each function.
 
-I have changed the naming convention to comply with pep8: 
-Switch from CamelCase to snake_case. 
-I've also simplified where possible. e.g. gross -> grs
-Standardized where possible. e.g. Mass, m -> ms; 
-Brought tot to front, add it where in description, but missing
-There is an inconsistency with tot & yearly use. Need to check all names 
-Min reserved from mineralization. Minimum changed to minim
+Values are initially set to zero.
+
+Naming convention:
+*Changed to comply with pep8: Switch from CamelCase to snake_case. 
+*Simplified where possible. e.g. gross -> grs
+*Standardized where possible. e.g. Mass, m -> ms; tot to front
+*There is an inconsistency with tot & yearly use. When should we use tot?
+*Min reserved from mineralization. Minimum changed to minim
 
 '''
 share = {
