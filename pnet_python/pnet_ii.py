54b4deeb9cd4634253bbb943b92bf4bd07787ebd
Jack-Hastings <Jack.Hastings@gmail.com>
2020-06-04 17:18:01 -0400

    successful run with atmenv, phenol

--- pnet_python/pnet_ii.py
+++ pnet_python/pnet_ii.py
@@ -1,6 +1,7 @@
 #Climate clim file
-from pnet_input import share
+from pnet_input import share, veg, clim, site
 from AtmEnviron import atm_environ
+from phenology import phenology
 
 #for rstep in range(input.clim_length): # steps through each climate.clim input line
 #   AtmEnviron.atm_environ(rstep)
@@ -12,7 +13,8 @@ for testing purposes
 '''
 clim_length = 5
 
-for i in range(clim_length):
-    atm_environ(i, share)
+for rstep in range(clim_length):
+    atm_environ(rstep, share, clim, site)
+    phenology(rstep, veg, clim, share, growthphase = 1, timestep = 0)
 
 print(share)
