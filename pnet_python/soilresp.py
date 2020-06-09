8e1299095a6ba61c9201d50ef0b18628a91d7913
Jack-Hastings <Jack.Hastings@gmail.com>
2020-06-09 17:41:18 -0400

    soil_rsp__ pnet_ii

---
+++ pnet_python/soilresp.py
@@ -0,0 +1,16 @@
+from math import exp
+
+
+def soilresp(veg, share, rstep):
+    '''soil respiration routine for pnet ii only?'''
+
+    #end of equation ... whats the point of the plus zero?
+    share['soil_rsp_mo'] = veg['soil_rsp_a'] * exp(veg['soil_rsp_b'] * (share['t_ave'] + 0)) # g C/m2.mo
+    share['soil_rsp_mo'] = share['soil_rsp_mo'] * share['mean_soil_moist_eff']
+    share['soil_rsp_mo'] = share['soil_rsp_mo'] * (share['dayspan'] / 30.5) #g C/m2.DaySpan
+    share['soil_rsp_yr'] = share['soil_rsp_yr'] + share['soil_rsp_mo']
+
+    '''Besides being a bit harder to follow
+
+    Couldn't the first three lines of this be lumped into one long equation?
+    '''
