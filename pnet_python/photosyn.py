3b896e784aef5ef93e1506f12a4df93a3e6827e1
Jack-Hastings <Jack.Hastings@gmail.com>
2020-06-09 10:47:05 -0400

    ok

--- pnet_python/photosyn.py
+++ pnet_python/photosyn.py
@@ -21,7 +21,7 @@ def photosyn(clim, share, site, veg, rstep):
         d_temp = 0
 
     share['dvpd'] = 1.0 - veg['dvpd1'] * (pow(share['vpd'], veg['dvpd2']))
-    #ten9 = 1000000000.0 # this seems silly. I'm going to use pow(10,9)
+    # ten9 = 1000000000.0 # this seems silly. I'm going to use pow(10,9)
 
     '''In c++
     Set atmospheric co2 concentration 
@@ -52,7 +52,7 @@ def photosyn(clim, share, site, veg, rstep):
     ca_0 = 350
     # share['d_amax'] = (clim.loc[rstep, 'co2_atm'] - gamma) / \
     #    (clim.loc[rstep, 'co2_atm'] + 2 * gamma) * (ca_0 + 2 * gamma) / \
-    #        (ca_0 - gamma)  
+    #        (ca_0 - gamma)
     # (Franks,2013, New Phytologist, 197:1077-1094)
 
     '''Calculate co2 effect on conductance 
@@ -68,30 +68,40 @@ def photosyn(clim, share, site, veg, rstep):
         gs_int = (0.4656 * share['d_amax']) - 0.9701
 
         # d_gs = share['d_amax'] / (clim.loc[rstep, 'co2_atm'] / ca_0)
-        # share['d_wue'] = 1 / d_gs 
+        # share['d_wue'] = 1 / d_gs
         # (Franks,2013, New Phytologist, 197:1077-1094) need to refine with the downregulation
     else:
         share['d_wue'] = 1
         gs_slope = (-0.6157 * share['d_amax']) + 1.4582
         gs_int = (0.4974 * share['d_amax']) - 0.9893
 
-
     f_amax = 1
     #f_amax = 1.46 //hw
 
-    amax = (veg['amax_a'] + veg['amax_b'] * veg['fol_n_con']) * share['d_amax'] * f_amax # nmole CO2/g Fol.sec
-    # amax = (veg['amax_a'] + veg['amax_b'] * 1.8) * share['d_amax'] * f_amax # nmole CO2/g Fol.sec 
-    # amax = pow(10.0, (0.74 * log10(veg['fol_n_con']) - 0.57 * log10(slw_layer) + 2.96)) * share['d_amax'] # nmole CO2/g Fol.sec 
+    amax = (veg['amax_a'] + veg['amax_b'] * veg['fol_n_con']) * \
+        share['d_amax'] * f_amax  # nmole CO2/g Fol.sec
+    # amax = (veg['amax_a'] + veg['amax_b'] * 1.8) * share['d_amax'] * f_amax # nmole CO2/g Fol.sec
+    # amax = pow(10.0, (0.74 * log10(veg['fol_n_con']) - 0.57 * log10(slw_layer) + 2.96)) * share['d_amax'] # nmole CO2/g Fol.sec
     # veg['base_fol_rsp_frac'] = 0.14 - 0.002 * share['t_ave'] # longterm leaf respiration acclimation (wythers, 2013, JGR, V118, 1?4)
     # veg['rsp_q10] = 3.22 - 0.046 * (share['t_ave'] + veg['psn_t_opt'])/2 # longterm leaf respiration acclimation (wythers, 2013, JGR, V118, 1?4)
     # veg['base_fol_rsp_frac'] = 0.09
     base_fol_rsp = veg['base_fol_rsp_frac'] * amax * veg['fol_rsp_frac']
     amax = amax * veg['amax_frac']
     grs_amax = amax + base_fol_rsp
-    # grs_amax: g C/g Fol/day, 12 for C atom 
-    grs_amax = (grs_amax * share['dvpd'] * d_temp * share['day_length'] * 12) / pow(10,9)
+    # grs_amax: g C/g Fol/day, 12 for C atom
+    grs_amax = (grs_amax * share['dvpd'] * d_temp *
+                share['day_length'] * 12) / pow(10, 9)
 
     if grs_amax < 0:
         grs_amax = 0
 
+    # g C/g Fol/day
+    share['day_rsp'] = (base_fol_rsp * (pow(veg['rsp_q10'], ((share['t_day'] -
+                                                              veg['psn_t_opt']) / 10))) * share['day_length'] * 12.0) / pow(10, 9)
+    share['day_rsp'] = (base_fol_rsp * (pow(veg['rsp_q10'], ((share['t_night'] -
+                                                              veg['psn_t_opt']) / 10))) * share['day_length'] * 12.0) / pow(10, 9)
+
+    '''Initialize ozone effect'''
+    can_net_psn_o3 = 0
+    can_net_psn_pot = 0
     
